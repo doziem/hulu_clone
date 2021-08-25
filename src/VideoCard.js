df4378fc531da86d84028d38b07a08928d6857d9
Doziem <agbokennethchidoziem@gmail.com>
2021-08-25 02:03:19 +0100

    modification where made on the Results.css and VideoCard.css files

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -1,32 +1,30 @@
-import React from 'react'
-import './VideoCard.css'
+import React from 'react';
+import './VideoCard.css';
 import TextTruncate from 'react-text-truncate';
-import { ThumbUpSharp } from '@material-ui/icons'
+import { ThumbUpSharp } from '@material-ui/icons';
 
 const baseURL = 'https://image.tmdb.org/t/p/original';
 
 function VideoCard({ movie }) {
-    return (
-        <div className='videocard'>
-            <img src={`${baseURL}${movie.backdrop_path || movie.poster_path}`} alt='logo'
-            />
-            <div className='text'>
-                <TextTruncate
-                    line={1}
-                    element='p'
-                    text={movie.overview}
-                />
-            </div>
-            <h4>{movie.original_title}</h4>
+  return (
+    <div className="videocard">
+      <img
+        src={`${baseURL}${movie.backdrop_path || movie.poster_path}`}
+        alt="logo"
+      />
+      <div className="text">
+        <TextTruncate line={1} element="p" text={movie.overview} />
+      </div>
+      <h4>{movie.original_title}</h4>
 
-            <p className='videocard__stat'>
-                {movie.media_type && movie.media_type}
-                {movie.release_date || movie.first_air_date}.
-                <ThumbUpSharp />
-                {movie.vote_count}
-            </p>
-        </div>
-    )
+      <p className="videocard__stat">
+        {movie.media_type && movie.media_type}
+        {movie.release_date || movie.first_air_date}.
+        <ThumbUpSharp />
+        {movie.vote_count}
+      </p>
+    </div>
+  );
 }
 
-export default VideoCard
+export default VideoCard;
